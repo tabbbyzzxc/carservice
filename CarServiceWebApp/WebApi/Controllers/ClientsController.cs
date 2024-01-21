8069f89f71d26c46202f8740abe2990d2864c9f2
tabbbyzzxc <ipz221_mms@student.ztu.edu.ua>
2024-01-21 15:13:30 +0100

    Initial

---
+++ CarServiceWebApp/WebApi/Controllers/ClientsController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessEntities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        [HttpGet("clients-list")]
+        public async Task<List<Client>> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client { Id = 1, Name = "Vasya"},
+                new Client { Id = 2, Name = "Petya"}
+            };
+        }
+    }
+}
---
+++ CarServiceWebApp/WebApi/Program.cs
@@ -0,0 +1,48 @@
+namespace WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  policy =>
+                                  {
+                                      policy.WithOrigins("http://localhost:3000");
+                                  });
+            });
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors(MyAllowSpecificOrigins);
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
